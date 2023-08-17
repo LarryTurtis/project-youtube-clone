5d1bb998c31698198c7f67f8bb63583654ba9d9d
Kenneth-Parker <kennethkavon@gmail.com>
2023-08-17 15:57:08 -0400

    fun stuff n links

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ut from "./assets/youtube-logo-png-transparent-image-5.png"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,14 +10,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://youtu.be/ope1743gdAw" target="_blank">
+          <img src={ut} className="logo" alt="YouTube logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        {/* <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </a> */}
       </div>
-      <h1>Tyrell + Kenia + Ant + Natalie + Kenneth</h1>
+      <h1 className='ourNames'>Tyrell + Kenia + Ant + Natalie + Kenneth</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
